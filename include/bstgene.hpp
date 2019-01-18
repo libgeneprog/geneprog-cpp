39fd1c284acd072a28deb24aebf62ecbea1c8f96
Richard Wardin <shalmezad@gmail.com>
2019-01-18 10:27:50 -0500

    Work on documenting everything

--- include/bstgene.hpp
+++ include/bstgene.hpp
@@ -4,15 +4,34 @@
 #include "geneprog/gene.h"
 #include "geneprog/bst-data.h"
 
+/**
+ * A gene that represents a number of
+ * [binary syntax trees](https://en.wikipedia.org/wiki/Binary_expression_tree).
+ * The gene will have one tree per output.
+ */
 class BSTGene
 {
 private:
 	GP_Gene* gene;
 
 public:
+
+	/**
+	 * @param in The number of inputs the gene will receive during evaluation
+	 * @param depth The max depth for the tree
+	 * @param out The number of outputs the gene will make during evaluation
+	 */
 	BSTGene(int in, int depth, int out);
 	~BSTGene();
 
+	/**
+	 * Evaluates the binary syntax tree against the inputs,
+	 * and assigns the result to outputs.
+	 * *NOTE:* It is assumed that outputs was already declared, initialized,
+	 * and matches the number of outputs when the gene was built.
+	 * @param inputs The inputs to use for the syntax tree.
+	 * @param outputs A pointer to an array of where to store the results.
+	 */
 	void evaluate(double *inputs, double *outputs);
 };
 
--- include/cgpgene.hpp
+++ include/cgpgene.hpp
@@ -4,14 +4,31 @@
 #include "geneprog/gene.h"
 #include "geneprog/cgp-data.h"
 
+/**
+ * A gene that represents a [cartesian gene](https://www.cartesiangp.com/)
+ */
 class CGPGene
 {
 private:
 	GP_Gene* gene;
 
 public:
+	/**
+	 * @param in The number of inputs the gene will receive during evaluation
+	 * @param mid The number of middle nodes for the CGP gene
+	 * @param out The number of outputs the gene will make during evaluation
+	 */
 	CGPGene(int in, int mid, int out);
 	~CGPGene();
+
+	/**
+	 * Evaluates the cgp gene against the inputs,
+	 * and assigns the result to outputs.
+	 * *NOTE:* It is assumed that outputs was already declared, initialized,
+	 * and matches the number of outputs when the gene was built.
+	 * @param inputs The inputs to use for the syntax tree.
+	 * @param outputs A pointer to an array of where to store the results.
+	 */
 
 	void evaluate(double *inputs, double *outputs);
 };
